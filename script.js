1de82648de3b6276209b7d4da68be89315a6f823
tomutanyi <tomutanyi@gmail.com>
2023-08-03 17:16:06 +0300

    added submission form

--- script.js
+++ script.js
@@ -13,30 +13,57 @@ document.addEventListener("DOMContentLoaded", ()=>
     {
         for (const car of cars)
         {
-            let remaining = (car.carsleft)
+            let id = car.id
             let li = document.createElement('div')
             li.innerHTML=
             `
             <div class="card">
-                <img src="${car.picture}" alt="Avatar" style="width:100%">
+                <img src="${car.picture}" alt="Car Image" style="width:100%">
                 <div class="container">
                     <h4><b>${car.name}</b></h4> 
                     <p>${car.price}</p>
-                    <p id"carsleft">${car.carsleft}</p>
-                    <button id = "reserve" class= "reserve">Reserve</button>
+                    <p id = "carsleft${car.id}">${car.carsleft}</p>
+                    <button id = "reserve${car.id}" class= "reserve">Reserve</button>
                 </div>
             </div>
             `
             listcars.appendChild(li)
-            let reserve = document.getElementById("reserve")
-            let carsleft = document.getElementById("carsleft")
+
+            let reserve = document.querySelector(`#reserve${car.id}`)
+            let carsleft = document.querySelector(`#carsleft${car.id}`)
             reserve.addEventListener("click", ()=>
             {
-                remaining--
+                let remaining = parseInt(carsleft.textContent)
+                remaining = parseInt(remaining)-1
                 carsleft.innerText = `${remaining}`
-
             }) 
         }
     }
+
+    function addcar()
+    {
+        fetch("http://localhost:3000/cars", {
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify({
+                price:document.getElementById("prices").value,
+                name:document.getElementById("carname").value,
+                carsleft:document.getElementById("carsleft").value,
+                color:document.getElementById("color").value,
+                company:document.getElementById("company").value,
+                picture:document.getElementById("picture").value
+        })
+        })
+        .then(response=>response.json())
+        .then(data=>console.log(data))
+        .catch(error=>console.log(error))
+    }
+
+    let carform = document.getElementById("carsubmit")
+    carform.addEventListener("submit",function(event){
+    event.preventDefault()
+    addcar()
+    carform.reset() 
+})
 
 })
