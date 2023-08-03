02aa50168057940e0705af7b3fdee977d129ea4c
tomutanyi <tomutanyi@gmail.com>
2023-08-04 02:00:36 +0300

    added available cars title to the card

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", ()=>
                 <div class="container">
                     <h4><b>${car.name}</b></h4> 
                     <p>${car.price}</p>
+                    <h3>Available cars</h3>
                     <p id = "carsleft${car.id}">${car.carsleft}</p>
                     <button id = "reserve${car.id}" class= "reserve">Reserve</button>
                 </div>
